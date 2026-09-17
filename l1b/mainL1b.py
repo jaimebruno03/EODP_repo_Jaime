4e252e55bd2e94219d1f190537c6b56acdd3d82b
Jaime Bruno <jaimebruno03@gmail.com>
2026-09-17 17:46:56 +0200

    Class2_Start

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -6,7 +6,7 @@ from l1b.src.l1b import l1b
 # Directory - this is the common directory for the execution of the E2E, all modules
 auxdir = r'C:/GitHub/EODP_repo_Jaime/auxiliary'
 indir = r"C:/MasterEODP/EODP_TER_2021/EODP-TS-L1B/input"
-outdir = r"C:/MasterEODP/EODP_TER_2021/EODP-TS-L1B/myOutput"
+outdir = r"C:/MasterEODP/EODP_TER_2021/EODP-TS-L1B/myOutput_NotEq"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
